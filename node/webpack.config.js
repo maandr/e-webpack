f50d1dde5d48c86a530017fec59b02d35b5a676c
Mats Andresen <mandresen@papego.de>
2018-03-09 16:30:16 +0100

    minor cleanup

--- node/webpack.config.js
+++ node/webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const webpack = require("webpack")
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
+    context: path.resolve(__dirname, "src"),
     entry: {
-        "math.app": './app.js',
-        "math.lib": './lib.js'
+        "math.app": "./app.js",
+        "math.lib": "./lib.js"
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js' // [name] will be replace with the key of the corresponding entry
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js" // [name] will be replace with the key of the corresponding entry
     }
 };
